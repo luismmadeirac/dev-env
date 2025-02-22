75a9aa4bf4bb6f3a19f3bcbee84ad451ca480d35
luismmadeirac <eng.luismadeira@icloud.com>
2025-02-22 20:19:55 +0000

    change name from bootstrapt to mac-bootstrap

--- dev-env.sh
+++ dev-env.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-script_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
-dry="0"
+dry_run="0"
 
-while [[ $# > 0 ]]; do
-    if [[ "$1" == "--dry" ]]; then
-        dry="1"
-    fi
-    shift
-done
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    echo "no xdg config hom"
+    echo "using ~/.config"
+    XDG_CONFIG_HOME=$HOME/.config
+fi
+
+if [ -z "$DEV_ENV" ]; then
+    echo "env var DEV_ENV needs to be present"
+    exit 1
+fi
+
+if [[ $1 == "--dry" ]]; then
+    dry_run="1"
+fi
 
 log() {
     if [[ $dry == "1" ]]; then
@@ -30,6 +37,7 @@ execute() {
 log "--------- dev-env ---------"
 
 cd $script_dir
+
 copy_dir() {
     pushd $1
     to=$2
@@ -41,7 +49,7 @@ copy_dir() {
     popd
 }
 
-copy_dir .config $XDG_CONFIG_HOME
+# copy_dir .config $XDG_CONFIG_HOME
 
 copy_file() {
     from=$1
@@ -51,4 +59,13 @@ copy_file() {
     execute cp $from $to/$name
 }
 
-copy_file .specialrc $HOME
+# copy_file .specialrc $HOME
+
+# update_files $DEV_ENV/env/.config $XDG_CONFIG_HOME
+# update_files $DEV_ENV/env/.local $HOME/.local
+
+# copy $DEV_ENV/tmux-sessionizer/tmux-sessionizer $HOME/.local/scripts/tmux-sessionizer
+copy $DEV_ENV/env/.zsh_profile $HOME/.zsh_profile
+copy $DEV_ENV/env/.zshrc $HOME/.zshrc
+# copy $DEV_ENV/env/.tmux-sessionizer $HOME/.tmux-sessionizer
+# copy $DEV_ENV/dev-env $HOME/.local/scripts/dev-env
