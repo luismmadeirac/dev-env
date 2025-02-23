f2945ab9dcbba994184a18f099c6ba5ad7fd2406
luismmadeirac <eng.luismadeira@icloud.com>
2025-02-23 01:41:02 +0000

    dev env update

--- dev-env.sh
+++ dev-env.sh
@@ -5,26 +5,26 @@ dry_run="0"
 DEV_ENV="$HOME/personal/dev"
 
 if [ -z "$XDG_CONFIG_HOME" ]; then
-    echo "no xdg config hom"
-    echo "using ~/.config"
-    XDG_CONFIG_HOME=$HOME/.config
+  echo "no xdg config hom"
+  echo "using ~/.config"
+  XDG_CONFIG_HOME=$HOME/.config
 fi
 
 if [ -z "$DEV_ENV" ]; then
-    echo "env var DEV_ENV needs to be present"
-    exit 1
+  echo "env var DEV_ENV needs to be present"
+  exit 1
 fi
 
 if [[ $1 == "--dry" ]]; then
-    dry_run="1"
+  dry_run="1"
 fi
 
 log() {
-    if [[ $dry == "1" ]]; then
-        echo "[DRY_RUN]: $@"
-    else
-        echo "$@"
-    fi
+  if [[ $dry == "1" ]]; then
+    echo "[DRY_RUN]: $@"
+  else
+    echo "$@"
+  fi
 }
 
 log "env: $DEV_ENV"
@@ -32,37 +32,37 @@ log "------------------ dev-env ------------------"
 log ""
 
 update_files() {
-    log "copying over files from: $1"
-    pushd $1 &>/dev/null
-    (
-        configs=$(find . -mindepth 1 -maxdepth 1 -type d)
-        for c in $configs; do
-            directory=${2%/}/${c#./}
-            log "    removing: rm -rf $directory"
+  log "copying over files from: $1"
+  pushd $1 &>/dev/null
+  (
+    configs=$(find . -mindepth 1 -maxdepth 1 -type d)
+    for c in $configs; do
+      directory=${2%/}/${c#./}
+      log "    removing: rm -rf $directory"
 
-            if [[ $dry_run == "0" ]]; then
-                rm -rf $directory
-            fi
+      if [[ $dry_run == "0" ]]; then
+        rm -rf $directory
+      fi
 
-            log "    copying env: cp $c $2"
-            if [[ $dry_run == "0" ]]; then
-                cp -r ./$c $2
-            fi
-        done
+      log "    copying env: cp $c $2"
+      if [[ $dry_run == "0" ]]; then
+        cp -r ./$c $2
+      fi
+    done
 
-    )
-    popd &>/dev/null
+  )
+  popd &>/dev/null
 }
 
 copy() {
-    log "removing: $2"
-    if [[ $dry_run == "0" ]]; then
-        rm $2
-    fi
-    log "copying: $1 to $2"
-    if [[ $dry_run == "0" ]]; then
-        cp $1 $2
-    fi
+  log "removing: $2"
+  if [[ $dry_run == "0" ]]; then
+    rm $2
+  fi
+  log "copying: $1 to $2"
+  if [[ $dry_run == "0" ]]; then
+    cp $1 $2
+  fi
 }
 
 # Essentially all the contents in here would be already in personal/dev becasue of the git clone
