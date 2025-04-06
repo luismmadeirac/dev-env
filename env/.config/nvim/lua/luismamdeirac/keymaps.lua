36abe441dc0b26be51f633ee40b681011c62b305
luismmadeirac <eng.luismadeira@icloud.com>
2025-04-06 21:11:43 +0100

    nvim configs updates

--- env/.config/nvim/lua/luismamdeirac/keymaps.lua
+++ env/.config/nvim/lua/luismamdeirac/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader><leader>", function()
+vim.keymap.set("n", "<leader>so", function()
   vim.cmd("so")
 end)
+
+local keymap = vim.keymap
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
