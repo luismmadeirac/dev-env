34de79ca45d623f356530d7ad2fca87561c39c4e
luismmadeirac <eng.luismadeira@icloud.com>
2025-04-16 18:49:12 +0100

    deprecated aws credentials configuration

--- scripts/aws-credentials.sh
+++ scripts/aws-credentials.sh
@@ -32,19 +32,26 @@ get_current_version() {
   aws --version | cut -d ' ' -f 1 | cut -d '/' -f 2
 }
 
-configure_aws_cli() {
-  log "Would you like to configure AWS CLI credentials?"
-  log "You can also run (aws configure sso) for configuring aws credentials with your IAM Identity Center profile"
-  read -p "Configure AWS CLI now? (y/n) " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    aws configure
-    if [ $? -eq 0 ]; then
-      log "AWS CLI configured successfully!"
-    else
-      log "Failed to configure AWS CLI. Please try again manually using 'aws configure'"
-    fi
-  fi
-}
 
-configure_aws_cli
+#
+# WARNING
+# Implementation Deprecated currently using aws sso
+# Need to update this script to use both older credentials file or new sso implementation from IAM
+#
+#
+# configure_aws_cli() {
+#   log "Would you like to configure AWS CLI credentials?"
+#   log "You can also run (aws configure sso) for configuring aws credentials with your IAM Identity Center profile"
+#   read -p "Configure AWS CLI now? (y/n) " -n 1 -r
+#   echo
+#   if [[ $REPLY =~ ^[Yy]$ ]]; then
+#     aws configure
+#     if [ $? -eq 0 ]; then
+#       log "AWS CLI configured successfully!"
+#     else
+#       log "Failed to configure AWS CLI. Please try again manually using 'aws configure'"
+#     fi
+#   fi
+# }
+
+
